a3791e4f948fbf64a9d4dfdb573f8ad813be8834
Elena <emkhuzhina@gmail.com>
2024-08-06 01:49:43 +0530

    add components for shared and ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>Home
-      <Button variant="outline">Корзина</Button>
-    </h1>
-
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+        <TopBar />
+      </Container>
+    </>
   );
 }
---
+++ components/shared/index.ts
@@ -0,0 +1,8 @@
+export { Title } from './title';
+export { Container } from './container';
+export { Header} from './header'; 
+export { Categories } from './categories';
+export {SortPopup} from './sort-popup'; 
+export { TopBar } from './top-bar';
+
+
